83b2b802c915a4ebaccc34ed0c34518cbc373834
Colin de Vries <colindevries325@gmail.com>
2020-03-03 13:34:14 -0800

    pulling changes from master

--- adv.py
+++ adv.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import sys
+import datetime
+import time
 
 INITIALIZE_URL = "https://lambda-treasure-hunt.herokuapp.com/api/adv/init/"
 MOVEMENT_URL = "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/"
@@ -33,7 +35,10 @@ class Stack():
 
 
 baseUrl = 'https://lambda-treasure-hunt.herokuapp.com'
-auth = {"Authorization": "Token 2330ee34073008c724a7066470b88940e7278f5c"}
+colin_auth = {
+    "Authorization": "Token 2330ee34073008c724a7066470b88940e7278f5c"}
+# eli_auth
+auth = {"Authorization": "Token a39b9cc5b11d49d162694cfee69a4710093b2106"}
 
 
 def init():
@@ -41,7 +46,10 @@ def init():
     data = requests.get(baseUrl + INITIALIZE_URL, headers=auth)
 
     if data != None:
+        print("DATA: ", data.json())
+        time.sleep(data.json()['cooldown'])
         return data.json(), 200
+
     else:
         return ({
             'Error': 'No data returned.'
@@ -66,6 +74,7 @@ def move(direction):
     directionData = {'direction': direction}
     data = requests.post(baseUrl + MOVEMENT_URL,
                          headers=auth, json=directionData).json()
+    time.sleep(data['cooldown'])
 
     if data != None:
         return data, 200
@@ -155,6 +164,7 @@ backtrack_directions = []
 # Create a stack and push current data onto the stack
 stack = Stack()
 current_data = init()
+
 stack.push(current_data)
 
 # While there are items in the stack...
@@ -168,29 +178,34 @@ while stack.size() > 0:
     if room_id not in visited:
         visited[room_id] = {"n": "?", "s": "?", "e": "?", "w": "?"}
 
-    # For each exit of current room, check if the exit has been explored
-    for d in room_exits:
-        # If direction has not been explored, move that direction and set all_exits_explore = False
-        if visited[d] == "?":
-            all_exits_explored = False
-            new_data = move(d)
-            # Push opposite direction to backtrack_directions list
-            backtrack_directions.push(opposites[d])
-            # Get new room id and add it to the direction value of the current room in visited
-            new_room_id = new_data[0].get('room_id')
-            visited[room_id][d] = new_room_id
+    if len(room_exits) != 0:
+        # For each exit of current room, check if the exit has been explored
+        for d in room_exits:
+            # If direction has not been explored, move that direction and set all_exits_explore = False
+            if visited[room_id][d] == "?":
+                all_exits_explored = False
+                print("Direction", d)
+                new_data = move(d)
+                # Push opposite direction to backtrack_directions list
+                backtrack_directions.append(opposites[d])
+                # Get new room id and add it to the direction value of the current room in visited
+                new_room_id = new_data[0].get('room_id')
+                visited[room_id][d] = new_room_id
 
-            # If new room is not in visited, add it
-            if new_room_id not in visited:
-                visited[new_room_id] = {"n": "?", "s": "?", "e": "?", "w": "?"}
-                # Add the last room's id to the current room's values in visited
-                visited[new_room_id][opposites[d]] = room_id
-            # Push new room onto the stack
-            stack.push(new_data)
+                # If new room is not in visited, add it
+                if new_room_id not in visited:
+                    visited[new_room_id] = {
+                        "n": "?", "s": "?", "e": "?", "w": "?"}
+                    # Add the last room's id to the current room's values in visited
+                    visited[new_room_id][opposites[d]] = room_id
+                # Push new room onto the stack
+                stack.push(new_data)
+    print(f"Visited: {visited}, Backtrack_Directions: {backtrack_directions}")
 
     # If all exits have been explored for the current room, backtrack and add room onto the stack
     if all_exits_explored == True:
         new_data = move(backtrack_directions.pop())
+
         stack.push(new_data)
 
 
